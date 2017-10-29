0e390e41c1fa3cbb013542eae21eace419f94454
craig <craigcliff@gmail.com>
2017-10-29 20:10:11 +0200

    First commit

---
+++ custom.js
@@ -0,0 +1,6 @@
+const navBar = document.querySelector('.nav--page-header');
+const navBtn = document.querySelector('.nav-btn');
+navBtn.addEventListener('click', function() {
+  this.classList.toggle('open');
+  navBar.classList.toggle('nav--open');
+});
